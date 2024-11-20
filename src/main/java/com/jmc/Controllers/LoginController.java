997b25a6033259eff6d0c3b99684e4ba4ce1503a
Sandile Kitayi The Developer <ksandile127@gmail.com>
2024-11-20 15:53:12 +0200

    ss

--- src/main/java/com/jmc/Controllers/LoginController.java
+++ src/main/java/com/jmc/Controllers/LoginController.java
@@ -25,24 +25,30 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         acc_selector.setItems(FXCollections.observableArrayList(AccountType.CLIENT, AccountType.ADMIN));
         acc_selector.setValue(Model.getInstance().getViewFactory().getLoginAccountType());
-        acc_selector.valueProperty().addListener(observable -> Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue()));
+
+        // Use _unused for the listener parameter
+        acc_selector.valueProperty().addListener((_unused) ->
+                Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue())
+        );
+
+        // Handle login button action
         login_btn.setOnAction(e -> onLogin());
     }
+
     private void onLogin() {
         Stage stage = (Stage) error_lbl.getScene().getWindow();
         if (Model.getInstance().getViewFactory().getLoginAccountType() == AccountType.CLIENT) {
-            // Evaluate Client Login Cred
+            // Evaluate Client Login Credentials
             Model.getInstance().evaluateClientCredentials(payee_address_fld.getText(), password_fld.getText());
-            if (Model.getInstance().getClientLoginSuccessFlag()){
+            if (Model.getInstance().getClientLoginSuccessFlag()) {
                 Model.getInstance().getViewFactory().showClientWindow();
-                //Close the login page
+                // Close the login page
                 Model.getInstance().getViewFactory().closeStage(stage);
             } else {
                 payee_address_fld.setText("");
                 password_fld.setText("");
                 error_lbl.setText("No such login credentials.");
             }
-
         } else {
             Model.getInstance().getViewFactory().showAdminWindow();
         }
